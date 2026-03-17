c4e0f5c51501cfc07552408ded723f02b611eef9
Maksym Stoianov <stoianov.maksym@gmail.com>
2026-03-17 22:09:30 +0100

    fix: format

--- src/appsscript/sheet/appendColumns.ts
+++ src/appsscript/sheet/appendColumns.ts
@@ -3,6 +3,7 @@ import { isConsistent2DArray } from "../../lang";
 import { requireSheet } from "./requireSheet";
 
 export interface Options {
+
   /**
    * Determines whether to insert columns after frozen columns.
    * If `true`, columns will be added immediately to the right of any frozen columns, if they exist.
--- src/appsscript/sheet/types/GridRange.ts
+++ src/appsscript/sheet/types/GridRange.ts
@@ -5,6 +5,7 @@
  * @version 1.0.0
  */
 export interface GridRange {
+
   /**
    * The sheets this range is on.
    */
